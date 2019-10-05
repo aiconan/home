ec4dcfa982dc262aeafe7ec39ae45db5c66a2ada
AiConan <3025945576@qq.com>
2019-10-05 13:06:58 +0800

    Commit

---
+++ js/app.js
@@ -0,0 +1,80 @@
+window.onload = function(){
+    var app = new Vue({
+        el: '#app',
+        data: {
+            menu: false,
+            list: list,
+            s_list: s_list,
+            s_choose: false,
+            s_text: null,
+            s_now: 'baidu',
+            s_sug: false
+        },
+        methods: {
+            ajax: function(obj){
+                obj = obj || {};
+                var xmlhttp, type, url, async, dataType, data;
+                type = obj.type || 'GET';
+                type = trim(type).toUpperCase();
+                url = obj.url
+                url = trim(url);
+                async = obj.async || true;
+                dataType = obj.dataType || 'HTML';
+                dataType = trim(dataType).toUpperCase();
+                data = obj.data || {};
+                function trim(str) {
+                    return str.replace(/^\s+|\s+$/g, "");
+                };
+                var formatParams = function() {
+                    if (typeof(data) == "object") {
+                        var str = "";
+                        for (var pro in data) {
+                            str += pro + "=" + data[pro] + "&";
+                        }
+                        data = str.substr(0, str.length - 1);
+                    }
+                    if (type == 'GET' || dataType == 'JSONP') {
+                        if (url.lastIndexOf('?') == -1) {
+                            url += '?' + data;
+                        } else {
+                            url += '&' + data;
+                        }
+                    }
+                }
+                if (window.XMLHttpRequest) {
+                    xmlhttp = new XMLHttpRequest();
+                } else {
+                    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                }
+                if (dataType == 'JSONP') {
+                    if (typeof(obj.beforeSend) == 'function') obj.beforeSend(xmlhttp);
+                    var callbackName = ('jsonp_' + Math.random()).replace(".", "");
+                    var oHead = document.getElementsByTagName('head')[0];
+                    data.cb = callbackName;
+                    var ele = document.createElement('script');
+                    ele.type = "text/javascript";
+                    oHead.appendChild(ele);
+                    window[callbackName] = function(json) {
+                        oHead.removeChild(ele);
+                        window[callbackName] = null;
+                        obj.success && obj.success(json);
+                    };
+                    formatParams();
+                    ele.src = url;
+                    return;
+                }
+            },
+            get_sug: function(t){
+                if(!t) return false;
+                this.ajax({
+                    url: 'http://unionsug.baidu.com/su?wd='+t,
+                    type: 'get',
+                    dataType: 'jsonp',
+                    success: function(data){
+                        if (data.s !== []) app.s_sug = data.s || false;
+                    }
+                })
+            }
+        }
+    })
+}
---
+++ js/config.js
@@ -0,0 +1,100 @@
+var list = {
+    邮箱: [
+        {
+            name: 'Gmail',
+            url: 'https://mail.google.com/mail/u/0/#inbox'
+        },{
+            name: 'Hotmail',
+            url: 'https://outlook.live.com/mail/'
+        },{
+            name: '网易邮箱',
+            url: 'https://mail.163.com/'
+        },{
+            name: 'QQ邮箱',
+            url: 'https://mail.qq.com/'
+        },{
+            name: '新浪邮箱',
+            url: 'https://mail.sina.com.cn/'
+        },{
+            name: '阿里邮箱',
+            url: 'https://qiye.aliyun.com/'
+        }
+    ],
+    开发: [
+        {
+            name: 'W3school',
+            url: 'https://www.w3school.com.cn/'
+        },{
+            name: 'Github',
+            url: 'https://www.github.com/'
+        },{
+            name: 'Codepen',
+            url: 'https://codepen.io/'
+        },{
+            name: '思否',
+            url: 'https://segmentfault.com/'
+        },{
+            name: '吾爱破解',
+            url: 'https://52pojie.cn/'
+        },{
+            name: 'CdnJs',
+            url: 'https://cdnjs.com/'
+        },{
+            name: 'Cloudflare',
+            url: 'https://dash.cloudflare.com/'
+        },{
+            name: 'MSDN下载',
+            url: 'https://msdn.itellyou.cn/'
+        },{
+            name: 'Vue.js',
+            url: 'https://cn.vuejs.org/'
+        }
+    ],
+    工具: [
+        {
+            name: 'IP查询',
+            url: 'https://ip.cn'
+        },{
+            name: 'Speedtest',
+            url: 'https://www.speedtest.net'
+        },{
+            name: '站长工具',
+            url: 'https://tool.chinaz.com/'
+        },{
+            name: '谷歌翻译',
+            url: 'https://translate.google.cn/'
+        },{
+            name: '酷安',
+            url: 'https://www.coolapk.com/'
+        },{
+            name: '网盘解析',
+            url: 'https://www.baiduwp.com/'
+        }
+    ]
+}
+var s_list = {
+        baidu: {
+            name: '百度',
+            icon: './icon/baidu.png',
+            action: 'https://www.baidu.com/s?ie=utf-8',
+            s_name: 'wd'
+        },
+        google: {
+            name: 'Google',
+            icon: './icon/google.png',
+            action: 'https://www.google.com/search?hl=zh-CN',
+            s_name: 'q'
+        },
+        360: {
+            name: '360',
+            icon: './icon/360.png',
+            action: 'https://www.so.com/s',
+            s_name: 'q'
+        },
+        duckduckgo: {
+            name: 'DuckDuckGo',
+            icon: './icon/duckduckgo.png',
+            action: 'https://www.duckduckgo.com/s',
+            s_name: 'q'
+        }
+    }
